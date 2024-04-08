2c99f1b512160497cc0bb70d39bffb280398a8e3
Daniel Puerta <daniwar3@gmail.com>
2024-04-07 20:08:28 -0500

    se termina version basica

--- src/Context/Index.jsx
+++ src/Context/Index.jsx
@@ -2,7 +2,7 @@ import { createContext, useState,useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
-export const ShoppingCartProvider = ({ children }) => {
+export const ShoppingCartProvider = ({children}) => {
   // Shopping Cart · Increment quantity
   const [count, setCount] = useState(0)
 
@@ -24,15 +24,53 @@ export const ShoppingCartProvider = ({ children }) => {
 
   // Shopping Cart · Order
   const [order, setOrder] = useState([])
-  //get products
+
+  // Get products
   const [items, setItems] = useState(null)
+  const [filteredItems, setFilteredItems] = useState(null)
+
+  // Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null)
+  const [searchByCategory, setSearchByCategory] = useState(null)
 
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
       .then(response => response.json())
       .then(data => setItems(data))
   }, [])
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
 
 
   return (
@@ -54,8 +92,10 @@ export const ShoppingCartProvider = ({ children }) => {
       items,
       setItems,
       searchByTitle,
-      setSearchByTitle
-
+      setSearchByTitle,
+      filteredItems,
+      searchByCategory,
+      setSearchByCategory
     }}>
       {children}
     </ShoppingCartContext.Provider>
